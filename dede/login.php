ed860ea7bac4d98a9d198438d3202f189c33484d
wocaca <woca@qq.com>
2014-07-14 21:09:56 +0800

    no message

--- dede/login.php
+++ dede/login.php
@@ -63,11 +63,13 @@ if($dopost=='login')
 {
     $validate = empty($validate) ? '' : strtolower(trim($validate));
     $svali = strtolower(GetCkVdValue());
-    if(($validate=='' || $validate != $svali) && preg_match("/6/",$safe_gdopen)){
+    //if(($validate=='' || $validate != $svali) && preg_match("/6/",$safe_gdopen)){
+	if(false){
         ResetVdValue();
         ShowMsg('验证码不正确!','login.php',0,1000);
         exit;
-    } else {
+    }	
+	else {
         $cuserLogin = new userLogin($admindir);
         if(!empty($userid) && !empty($pwd))
         {
